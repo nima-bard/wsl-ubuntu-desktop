833cf09c63dcdf819f12653342f1ea5e4bba5a43
Nima Boobard <nima.boobard@gmail.com>
2020-06-23 17:48:59 +0430

    unnecessary SUDO removed

--- gnome-desktop.sh
+++ gnome-desktop.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+
 /mnt/c/Program\ Files/VcXsrv/vcxsrv.exe -ac -wgl -fullscreen -dpms&
-sudo /etc/init.d/dbus restart;
-sudo /etc/init.d/x11-common restart;
-sudo gnome-shell --x11 -r --display=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-~                                                                                                                                    ~                                                                          
+/etc/init.d/dbus restart;
+/etc/init.d/x11-common restart;
+gnome-shell --x11 -r --display=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
